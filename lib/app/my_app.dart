7f8aacebc0cfd1c513a219dc35110f415ab44c0a
Kevin-Sarges <kevinsarges08@gmail.com>
2023-04-03 17:23:38 -0300

    debugShowCheckedModeBanner

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Unamaps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
